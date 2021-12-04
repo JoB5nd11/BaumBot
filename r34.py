19d34ca3aef3072ab0bcf0f7feabe46f7a153858
JoB5nd11 <johannesbendler115@gmail.com>
2021-12-04 13:57:10 +0100

    R34 Gay Mario now works

--- r34.py
+++ r34.py
@@ -1,9 +1,11 @@
 import rule34
 import random
+import nest_asyncio
+nest_asyncio.apply()
+
+r34 = rule34.Sync()
 
 def getr34img(search):
-
-    r34 = rule34.Sync()
     images = r34.getImages(search)
 
     links = []
